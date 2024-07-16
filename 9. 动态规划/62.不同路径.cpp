b8651b3fd3758d3a5f1c2067ed56460c3cd5a93f
channinghsu <xbc1241888704@gmail.com>
2024-07-16 16:44:08 +0800

    update leetcode

--- 9. 动态规划/62.不同路径.cpp
+++ 9. 动态规划/62.不同路径.cpp
@@ -10,14 +10,12 @@ class Solution {
   int uniquePaths(int m, int n) {
     // dp 数组代表第[i][j]的格子的路径和
     vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-
     for (int i = 1; i <= m; i++) {
       dp[i][1] = 1;
     }
     for (int j = 1; j <= n; j++) {
       dp[1][j] = 1;
     }
-
     for (int i = 2; i <= m; ++i) {
       for (int j = 2; j <= n; ++j) {
         dp[i][i] = dp[i - 1][j] + dp[i][j - 1];
