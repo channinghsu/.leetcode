99539f10831fd68d3faf6cb05e961ce3e7a82e57
channinghsu <xbc1241888704@gmail.com>
2024-07-26 20:08:00 +0800

    Update reverseString function to use STL reverse for better performance

--- 4. 字符串/344.反转字符串.cpp
+++ 4. 字符串/344.反转字符串.cpp
@@ -4,23 +4,17 @@
  * [344] 反转字符串
  */
 
-// @lc code=start
+ // @lc code=start
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        // 库函数 reverse
-        // reverse(s.begin(), s.end());
-        // 双指针
-        // 若为一个字符，直接返回
-        if (s.size() == 1) return;
-        // 定义快慢指针
-        int slow = 0;
-        int fast = s.size() - 1;
-        // 首尾交换
-        while ( slow < fast) {
-            swap(s[slow], s[fast]);
-            slow++;
-            fast--;
+        int n = s.size();
+        int left = 0;
+        int right = n - 1;
+        for (int i = 0; i < n / 2; i++) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
         }
     }
 };
--- 4. 字符串/541.反转字符串-ii.cpp
+++ 4. 字符串/541.反转字符串-ii.cpp
@@ -4,18 +4,22 @@
  * [541] 反转字符串 II
  */
 
-// @lc code=start
+ // @lc code=start
 class Solution {
-  public:
-    string reverseStr(string s, int k) {
-        for (int i = 0; i < s.size(); i += 2 * k) {
-            if (i + k <= s.size()) {
-                reverse(s.begin() + i, s.begin() + i + k);
-            } else {
-                reverse(s.begin() + i, s.end());
-            }
+public:
+    std::string reverseStr(std::string s, int k) {
+        int n = s.size();
+
+        // 遍历每个 2k 块
+        for (int i = 0; i < n; i += 2 * k) {
+            // 计算当前块的结束位置，避免越界
+            int end = std::min(i + k, n);
+            // 使用 STL 库的 reverse 函数反转当前块
+            std::reverse(s.begin() + i, s.begin() + end);
         }
+
         return s;
     }
 };
+
 // @lc code=end
