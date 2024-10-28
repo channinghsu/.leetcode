ee1ff0576317e05f79841fc16566e7dc646315d8
channinghsu <xbc1241888704@gmail.com>
2024-10-28 10:17:55 +0800

    Refactor code for better readability and performance

--- 2. 链表/142.环形链表-ii.cpp
+++ 2. 链表/142.环形链表-ii.cpp
@@ -4,25 +4,26 @@
  * [142] 环形链表 II
  */
 
-// @lc code=start
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+ // @lc code=start
+ /**
+  * Definition for singly-linked list.
+  * struct ListNode {
+  *     int val;
+  *     ListNode *next;
+  *     ListNode(int x) : val(x), next(NULL) {}
+  * };
+  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-        ListNode* slow = head;
+    ListNode* detectCycle(ListNode* head) {
         ListNode* fast = head;
+        ListNode* slow = head;
 
-        while (fast!= nullptr && fast->next != nullptr) {
-            slow = slow->next;
+        while (fast != nullptr && fast->next != nullptr) {
             fast = fast->next->next;
-            if (slow == fast) {
+            slow = slow->next;
+
+            if (fast == slow) {
                 ListNode* entry = head;
                 while (entry != slow) {
                     entry = entry->next;
